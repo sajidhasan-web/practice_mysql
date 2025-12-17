c32cb0dd63e52cb4c3f4e855de21e7908c2ce512
unknown <you@example.com>
2025-12-17 16:28:48 +0600

    limit and aliasing

--- group_by_and_order_by.sql
+++ group_by_and_order_by.sql
@@ -1,6 +1,13 @@
--- Group By And Order By
+-- Group By 
 
-SELECT gender 
-FROM employee_demographics
+SELECT gender, AVG(age), MIN(age), COUNT(age) 
+FROM employee_demographics 
 GROUP BY gender
+;
+
+-- Order By
+
+SELECT *
+FROM employee_demographics 
+ORDER BY gender, age DESC
 ;
